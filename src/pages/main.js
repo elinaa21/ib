78cdfb0c877c778ffb5672b55457fbdefd97662a
RinTheCat <liza@danilova.me>
2019-11-21 14:54:35 +0300

    Сделал компонент, добавил его в приложение + 2 css файла, папка с img

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import Header from '../components/header.js';
-
+import About from '../components/about/about.js';
 
 function Main() {
   return (
     <div>
         <Header main={true} />
+        <About main={true} />
         {/* THIS IS BIG COMPONENT, THAT CONSIST OF SMALLER ONES
             ADD THEM LIKE ONE ABOVE                           */}
     </div>
